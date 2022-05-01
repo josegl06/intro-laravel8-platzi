eae024339c294c1bd7c47aa8527341b1543a6cd3
ContaboServer <jose.gomez7@utp.ac.pa>
2022-05-01 16:01:55 -0500

    bd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Catergory;
+use App\Models\Course;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +19,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory(5)->create();
+        Catergory::factory(3)->create();
+        Course::factory(10)->create();
+        Post::factory(90)->create();
     }
 }
